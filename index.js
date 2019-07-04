c41bf17f927b61c016f47d38fd2ee7674f7f70ea
Charles Davis <calandavis1990@gmail.com>
2019-07-04 14:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyPrependKitten(prepAddName){
 function destructivelyRemoveLastKitten(killLastName){
   kittens.pop(killLastName)
 }
-function destructivelyRemoveFirstKitten(killFirstName){
-  kittens.shift(killFirstName)
+//function destructivelyRemoveFirstKitten(killFirstName){
+//  kittens.shift(killFirstName)
 }
