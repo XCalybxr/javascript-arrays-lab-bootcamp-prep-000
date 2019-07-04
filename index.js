038e8a78e516e8fab107c7b38f55db91a179a359
Charles Davis <calandavis1990@gmail.com>
2019-07-04 14:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name){
-  kittens.push(name);
+function destructivelyAppendKitten(postAddName){
+  kittens.push(postAddName);
 }
 function destructivelyPrependKitten(prepAddName){
   kittens.unshift(prepAddName);
 }
-function destructivelyRemoveLastKitten(killLastName)
+function destructivelyRemoveLastKitten(killLastName){
+  kittens.pop(killLastName)
+}
